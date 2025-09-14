df025cd7937f8336ef94066ad25685cfaad4519a
xgaslann <gorkem_aslan0@hotmail.com>
2025-09-14 14:28:46 +0300

    xgaslan initial

---
+++ src/main/java/com/xgaslan/kafkamicroservice/KafkaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.xgaslan.kafkamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xgaslan/kafkamicroservice/KafkaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgaslan.kafkamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
